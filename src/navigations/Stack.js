b43dc28e825a8cc5f7c6cc87fff58a627afa2f6a
bopboy2 <scp0000@gmail.com>
2021-10-26 06:50:53 +0900

    헤더 UI 수정

--- src/navigations/Stack.js
+++ src/navigations/Stack.js
@@ -1,15 +1,36 @@
 import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
 import { Home, List, Chat } from '../screens'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
 
 const Stack = createStackNavigator()
 
 const StackNav = () => {
     return (
-        <Stack.Navigator initialRouteName="Home" screenOptions={{ cardStyle: { backgroundColor: '#fff' } }}>
+        <Stack.Navigator
+            initialRouteName="Home"
+            screenOptions={{
+                cardStyle: { backgroundColor: '#fff' },
+                headerStyle: {
+                    height: 120,
+                    backgroundColor: '#555555',
+                    borderBottomWidth: 5,
+                    borderBottomColor: '#111111'
+                },
+                headerTitleStyle: {
+                    fontSize: 24,
+                    color: '#ffffff'
+                },
+                headerTitleAlign: 'center',
+                headerTintColor: '#ff6600',
+                headerTitle: ({ style }) => {
+                    return <MaterialCommunityIcons name="react" style={style}> RN</MaterialCommunityIcons>
+                }
+            }}
+        >
             <Stack.Screen name="List" component={List} />
-            <Stack.Screen name="Home" component={Home} />
-            <Stack.Screen name="Chat" component={Chat} />
+            <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
+            <Stack.Screen name="Chat" component={Chat} options={{ headerTitle: "Chat Screen" }} />
         </Stack.Navigator>
     )
 }
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,18 +1,24 @@
 import React from 'react'
 import styled from 'styled-components'
 import Button from '../components/Button'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
 const Container = styled.View`
     align-items:center;
     /* background-color: #ffffff; */
+    padding-top:${({ insets: { top } }) => top}px;
+    padding-bottom:${({ insets: { bottom } }) => bottom}px;
+    padding-right:${({ insets: { right } }) => right}px;
+    padding-left:${({ insets: { left } }) => left}px;
 `
 const StyledText = styled.Text`
     font-size: 30px;
     margin:10px;
 `
 const Home = ({ navigation }) => {
+    const insets = useSafeAreaInsets()
     return (
-        <Container>
+        <Container insets={insets}>
             <StyledText>Home</StyledText>
             <Button title="List" onPress={() => navigation.navigate("List")} />
         </Container>
