d1406182f80f43332684ba176bc3d67c6d9d488e
bopboy2 <scp0000@gmail.com>
2021-10-27 06:36:11 +0900

    탭바 스타일 수정

--- src/navigations/Tab.js
+++ src/navigations/Tab.js
@@ -1,15 +1,51 @@
 import React from "react"
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
 import { Mail, Profile, Settings } from "../screens"
+import { MaterialIcons } from '@expo/vector-icons'
 
 const Tab = createBottomTabNavigator()
-
+const TabBarIcon = ({ name, size, color }) => {
+    return <MaterialIcons name={name} size={size} color={color} />
+}
 const TabNav = () => {
     return (
-        <Tab.Navigator initialRouteName="Profile">
-            <Tab.Screen name="Mail" component={Mail} />
-            <Tab.Screen name="Profile" component={Profile} />
-            <Tab.Screen name="Settings" component={Settings} />
+        <Tab.Navigator
+            initialRouteName="Profile"
+            screenOptions={{
+                showLabel: true,
+                tabBarLabelPosition: 'below-icon',
+                tabBarStyle: {
+                    backgroundColor: '#444444',
+                    borderTopColor: '#0000ff',
+                    borderTopWidth: 3
+                },
+                tabBarActiveTintColor: '#ffffff',
+                tabBarInactiveTintColor: '#111111'
+            }}
+        >
+            <Tab.Screen
+                name="Mail" component={Mail}
+                options={{
+                    tabBarIcon: props => {
+                        return TabBarIcon({ ...props, name: props.focused ? 'mail' : 'mail-outline' })
+                    },
+                    tabBarLabel: 'Inbox'
+                }}
+            />
+            <Tab.Screen name="Profile" component={Profile}
+                options={{
+                    tabBarIcon: props => {
+                        return TabBarIcon({ ...props, name: props.focused ? 'person' : 'person-outline' })
+                    }
+                }}
+            />
+            <Tab.Screen name="Settings" component={Settings}
+                options={{
+                    tabBarIcon: props => {
+                        return TabBarIcon({ ...props, name: props.focused ? 'settings' : 'settings-applications' })
+                    }
+                }}
+            />
         </Tab.Navigator>
     )
 }
